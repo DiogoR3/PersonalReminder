ba3ef6f03558d833ccce3f449dd04250df060eac
Diogo Rodrigues <diogo.rodcarreira@hotmail.com>
2020-11-07 22:37:09 -0300

    Initial commit

---
+++ src/main/Menu.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Menu {
+
+	public void Start() {
+		
+		System.out.println("Please choose and option below:");
+		
+		//String option = System.console().readLine();
+		//System.out.println("Option = " + option);
+	}
+	
+}
---
+++ src/main/Program.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("**** Personal Reminder ****");
+		
+		Menu menu = new Menu();
+		menu.Start();
+	}
+}
